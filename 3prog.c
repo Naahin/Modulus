e85f755e4889de9e3a5aba732e7be7ca5ab2e077
Naahin <98544401+Naahin@users.noreply.github.com>
2022-03-28 22:05:48 +0600

    Add files via upload

---
+++ 3prog.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter value for a: ");
+    scanf("%d",&a);
+    printf("\nEnter value for b: ");
+    scanf("%d",&b);
+    c=a%b;
+    printf("\n%d",c);
+
+
+}
